8f574cc6b494d57daf7f488ad0cb737a04b6a41f
Dhanu Anjana <dhanu@orangehrm.com>
2024-11-04 11:42:12 +0530

    ENG-810: Remove unnecessary separator between consecutive page numbers in OXD pagination  (#801)
    * eng 810 - Fix pagination issue in product: Prevent unnecessary '...' separator display between consecutive page numbers
    
    * add log detail
    
    * add log details

--- components/src/core/components/Pagination/__tests__/pagination.spec.ts
+++ components/src/core/components/Pagination/__tests__/pagination.spec.ts
@@ -30,4 +30,54 @@ describe('Pageination.vue', () => {
       '<div role="option" class="oxd-select-option --selected"><span>50</span></div>',
     );
   });
+  it('shows the first separator when first page is missing and pageItems starts from 3 or higher', async () => {
+    const wrapper = mount(Pagination, {
+      props: {
+        length: 5,
+        current: 10,
+        max: 3,
+      },
+      data() {
+        return {
+          pageItems: [3, 4, 5],
+        };
+      },
+    });
+
+    expect(wrapper.find('.oxd-pagination-separator').exists()).toBe(true);
+  });
+
+  it('shows the second separator when last page is missing and last pageItem is less than (length - 1)', async () => {
+    const wrapper = mount(Pagination, {
+      props: {
+        length: 5,
+        current: 10,
+        max: 3,
+      },
+      data() {
+        return {
+          pageItems: [1, 2, 3],
+        };
+      },
+    });
+    const separators = wrapper.findAll('.oxd-pagination-separator');
+    expect(separators.length).toBe(1);
+    expect(separators[0].exists()).toBe(true);
+  });
+
+  it('does not show separators when pageItems contain both first and last pages', async () => {
+    const wrapper = mount(Pagination, {
+      props: {
+        length: 5,
+        current: 10,
+        max: 5,
+      },
+      data() {
+        return {
+          pageItems: [1, 2, 3, 4, 5],
+        };
+      },
+    });
+    expect(wrapper.find('.oxd-pagination-separator').exists()).toBe(false);
+  });
 });
