134690e84f7655814b5de09e94a105895efbbea7
Thinithi Dilshara <thinithi@orangehrm.com>
2024-11-27 17:22:46 +0530

    update list table skeleton loader

--- components/src/core/components/ListTable/ListTable.vue
+++ components/src/core/components/ListTable/ListTable.vue
@@ -4,7 +4,7 @@
       <oxd-card-tr :clickable="false">
         <oxd-card-th
           v-if="loading && skeleton"
-          class="oxd-padding-cell oxd-table-th"
+          class="oxd-padding-cell oxd-table-th checkbox-cell"
         >
           <oxd-skeleton class="oxd-skeleton-checkbox" animateHighlighted />
         </oxd-card-th>
